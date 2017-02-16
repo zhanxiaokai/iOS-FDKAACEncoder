8d132ab747a827f007e0cbf70a4e3b882c3b68e2
xiaokai.zhan <zhanxiaokai@changba.com>
2017-02-16 18:01:24 +0800

    Initial Commit

---
+++ FDKAACEncoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FDKAACEncoder
+//
+//  Created by apple on 2017/2/16.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
